307c7c764d053fc5735510f6e056922a2a5756a1
Jaime Segura <jseguraweb@gmail.com>
2020-02-18 15:39:08 +0100

    create github repos search app

---
+++ main.js
@@ -0,0 +1,42 @@
+// GitHub API => https://api.github.com/users/
+
+const userInput = document.querySelector('#user-input');
+const form = document.querySelector('form');
+const container = document.querySelector('.container');
+
+const gitHubData = async () => {
+    try {
+        let gitHubResponse = await fetch(`https://api.github.com/users/${userInput.value}/repos`);
+        let convertedData = await gitHubResponse.json();
+
+        convertedData.map(repo => {
+            container.style.display = 'none';
+            const item = document.createElement('div');
+            item.classList.add('repo');
+            const link = document.createElement('a');
+            link.classList.add('link');
+            link.setAttribute('href', `https://github.com/${userInput.value}/${repo.name}`);
+            link.appendChild(item);
+            const title = document.createElement('h3');
+            title.classList.add('title');
+            title.appendChild(document.createTextNode(repo.name));
+            const description = document.createElement('p');
+            description.classList.add('description');
+            description.appendChild(document.createTextNode(repo.description));
+            item.append(title, description);
+            container.appendChild(link);
+            container.style.display = 'flex';
+        })
+    } catch(err){
+        console.log(err);
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (userInput.value === '') {
+        alert('Please, enter username')
+    } else {
+        gitHubData();
+    }
+})
